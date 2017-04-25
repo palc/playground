c400466b98d232f527c3b1e64878dcc86dc65404
Tod Stuber <tod.p.stuber@usda.gov>
2017-04-25 06:34:43 -0500

    stats gathering correction

--- stats_get.sh
+++ stats_get.sh
@@ -2,8 +2,10 @@
 
 infile=$1
 
-read1=`grep -A 2 "fastq.gz file sizes" $infile | tail -1`
-read2=`grep -A 2 "fastq.gz file sizes" $infile | tail -2 | head -1`
+name=`echo $infile | sed 's/.stats.txt//'`
+
+read1=`grep -A 2 "^fastq.gz file sizes:" $infile | tail -1`
+read2=`grep -A 2 "^fastq.gz file sizes:" $infile | tail -2 | head -1`
 
 ave_coverage=`grep "Average.*coverage" $infile | awk '{print $NF}'`
 ref_with_coverage=`grep "eference.*coverage" $infile | awk '{print $NF}'`
@@ -11,7 +13,7 @@ ref_with_coverage=`grep "eference.*coverage" $infile | awk '{print $NF}'`
 mean_length=`grep -A 1 "Mean_Read_Length" $infile | tail -1`
 quality_snps=`grep -A 1 "SNPs of AC2" $infile | tail -1`
 
-printf "$read1\t$read2\t$ave_coverage\t$ref_with_coverage\t$mean_length\t$quality_snps\n"
+echo  "$name $read1 $read2 $ave_coverage $ref_with_coverage $mean_length $quality_snps"
 
 
 # tstuber 2017-04-19
